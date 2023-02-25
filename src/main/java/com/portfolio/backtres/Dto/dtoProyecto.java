4c9c8bb305b7641bd1e83ebc6b68369e89c33caf
richardfffff <richardmace6@gmail.com>
2023-02-25 00:26:10 -0300

    se realiza la parte de educacion y proyectos. probado, todo ok

---
+++ src/main/java/com/portfolio/backtres/Dto/dtoProyecto.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.backtres.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+
+public class dtoProyecto {
+    @NotBlank
+    private String nombreP;
+    @NotBlank
+    private String descripcionP; 
+
+    public dtoProyecto() {
+    }
+
+    public dtoProyecto(String nombreP, String descripcionP) {
+        this.nombreP = nombreP;
+        this.descripcionP = descripcionP;
+    }
+
+    public String getNombreP() {
+        return nombreP;
+    }
+
+    public void setNombreP(String nombreP) {
+        this.nombreP = nombreP;
+    }
+
+    public String getDescripcionP() {
+        return descripcionP;
+    }
+
+    public void setDescripcionP(String descripcionP) {
+        this.descripcionP = descripcionP;
+    }
+}
